7585e178945797484071d819bd431c08cc60221a
darcover4 <polesimov@gmail.com>
2026-07-08 20:00:53 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.darcover.visionassistant"
+    applicationId = "com.aistudio.visionassistant.bxzawe"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
